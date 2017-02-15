14c61c980b344ffa0126386b2b3fee7c564ed609
Wei Wang <wei.wang1@gmail.com>
2017-02-15 10:48:51 -0500

    update

---
+++ push-ecs-repo.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+#
+# push-ecs-repo.sh <aws-accountid> <aws-region> <ecr-repo>
+#
+AWS_ACCOUNT_ID=$1
+AWS_REGION = $2
+ECR_REPO = $3
+
+BUILD_VERSION=$(date +%s)
+
+eval $(aws --region ${AWS_REGION} ecr get-login)
+docker build -t ${ECR_REPO}:latest .
+echo "Deploying docker build version: ${BUILD_VERSION} to account: ${AWS_ACCOUNT_ID} ECR repo: ${ECR_REPO}"
+docker tag ${ECR_REPO}:latest ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO}:${BUILD_VERSION}
+echo Publish as ${ECR_REPO}:${BUILD_VERSION}
+docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO}:${BUILD_VERSION}
