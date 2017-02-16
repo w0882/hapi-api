c0a8354d2206d7a904a41d1c8f0151bbf8caf9c6
Wei Wang <wei.wang1@gmail.com>
2017-02-16 12:46:41 -0500

    update

--- deploy-ecs.sh
+++ deploy-ecs.sh
@@ -14,12 +14,12 @@ echo "### AWS_ACCOUNT_ID: ${AWS_ACCOUNT_ID}, AWS_REGION: ${AWS_REGION}, ECR_REPO
 #echo "### AWS_REGION: ${AWS_REGION}"
 #echo "### ECR_REPO: ${ECR_REPO}"
 #echo "### ECR_IMAGE_TAG: ${BUILD_VERSION}"
-
+echo "### Login to ECS docker env ..."
 eval $(aws --region ${AWS_REGION} ecr get-login)
 docker build -q -t ${ECR_REPO}:latest .
-echo "Deploying docker image with tag: ${BUILD_VERSION} to account: ${AWS_ACCOUNT_ID} ECR repo: ${ECR_REPO}"
+echo "### Deploying docker image with tag: ${BUILD_VERSION} to account: ${AWS_ACCOUNT_ID} ECR repo: ${ECR_REPO}"
 docker tag ${ECR_REPO}:latest ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO}:${BUILD_VERSION}
-echo Publish as ${ECR_REPO}:${BUILD_VERSION}
+echo "### Publishing image as ${ECR_REPO}:${BUILD_VERSION} to ECR ..."
 docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO}:${BUILD_VERSION}
 
 if [ $DEPLOY_TO_ECS_CLUSTER = 'true' ]; then
